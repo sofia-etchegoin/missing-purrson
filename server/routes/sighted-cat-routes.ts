9d67e9a322f97d59c54cd3ae5a081c818fc6b3c5
student <student@pop-os.localdomain>
2023-12-04 09:40:14 +1300

    clean up sighted cats route

--- server/routes/sighted-cat-routes.ts
+++ server/routes/sighted-cat-routes.ts
@@ -14,29 +14,7 @@ const storage = multer.diskStorage({
 const upload = multer({ storage })
 const router = Router()
 
-//GET localhost:5173/api/v1/sightedcats/
-// router.get('/', async (req, res) => {
-//   try {
-//     const sightedCats = await db.getAllSightedCatsDb()
-//     //console.log(cats)
-//     res.json(sightedCats)
-//   } catch (error) {
-//     console.log(error)
-//     res.status(500).json({ message: 'Something went wrong' })
-//   }
-// })
-
-// POST localhost:5173/api/v1/sightedcats/addcat
-// router.post('/addmissingcat', async (req, res) => {
-//   try {
-//     const newCat = await db.addSightedCatDb(req.body)
-//     res.status(201).json(newCat)
-//   } catch (err) {
-//     console.error('Error in POST /api/v1/addCat', err)
-//     res.status(500).json('Internal Server Error')
-//   }
-// })
-///api/v1/sightedcats/:catIdMc/add
+// Post localhost:5173/api/v1/sightedcats/:catIdMc/add
 router.post('/:catIdMc/add', upload.single('file'), async (req, res) => {
   try {
     if (!req.file) {
@@ -56,24 +34,7 @@ router.post('/:catIdMc/add', upload.single('file'), async (req, res) => {
   }
 })
 
-// GET localhost:5173/api/v1/singlecat/:catIdMc
-// router.get('/singlecat/:catIdMc', async (req, res) => {
-//   try {
-//     const catIdMc = Number(req.params.catIdMc)
-//     const sightedCats = await db.getOneSightedCatDb(catIdMc)
-//     //console.log(cats)
-//     res.json(sightedCats)
-//     if (!sightedCats) {
-//       res.status(404).json({ error: 'id could not be found' })
-//       return sightedCats
-//     }
-//   } catch (error) {
-//     console.log(error)
-//     res.status(500).json({ message: 'Something went wrong' })
-//   }
-// })
-
-// GET localhost:5173/api/v1/sightedcats/singlecat/sighting/:catIdMc
+// Get localhost:5173/api/v1/sightedcats/singlecat/sighting/:catIdMc
 router.get('/singlecat/sighting/:catIdMc', async (req, res) => {
   try {
     const catIdMc = Number(req.params.catIdMc)
