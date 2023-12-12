c39d2841b3e6acb5fcfe10fb9762f5ae8b44929b
Sofia Etchegoin <sofiaetchegoin@gmail.com>
2023-12-12 19:51:13 +1300

    hid google maps api key

--- server/server.ts
+++ server/server.ts
@@ -1,10 +1,14 @@
+//server.ts
 import express from 'express'
 import * as Path from 'node:path'
 
 import missingCatRoutes from './routes/missing-cat-routes'
 import sightedCatRoutes from './routes/sighted-cat-routes'
+import * as dotenv from 'dotenv'
 
 const server = express()
+
+dotenv.config()
 
 server.use(express.json())
 
@@ -18,5 +22,16 @@ if (process.env.NODE_ENV === 'production') {
     res.sendFile(Path.resolve('./dist/index.html'))
   })
 }
+
+// New route to fetch Google Maps API key
+server.get('/api/getGoogleMapsKey', (req, res) => {
+  const googleMapsAPIKey = process.env.GOOGLE_MAPS_API_KEY
+
+  if (googleMapsAPIKey) {
+    res.json({ apiKey: googleMapsAPIKey })
+  } else {
+    res.status(500).json({ error: 'Google Maps API key not found.' })
+  }
+})
 
 export default server
