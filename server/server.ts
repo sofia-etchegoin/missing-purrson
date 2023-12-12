9df3e8803fd771a85520487f8f2c6376155dc68d
Sofia Etchegoin <sofiaetchegoin@gmail.com>
2023-12-12 22:20:06 +1300

    modified server to use uploads folder

--- server/server.ts
+++ server/server.ts
@@ -17,7 +17,7 @@ server.use('/api/v1/sightedcats', sightedCatRoutes)
 if (process.env.NODE_ENV === 'production') {
   //server.use(express.static(Path.resolve('public')))
   server.use('/uploads', express.static('server/uploads'))
-  server.use('/assets', express.static(Path.resolve('./dist/assets')))
+  //server.use('/assets', express.static(Path.resolve('./dist/assets')))
   server.get('*', (req, res) => {
     res.sendFile(Path.resolve('./dist/index.html'))
   })
